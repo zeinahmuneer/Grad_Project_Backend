151299f2f030748853bb491eee4c6de35e653c34
Zeinah <zeinahadaileh@gmail.com>
2024-12-25 13:34:16 +0300

    -

--- api/routers/studentRouter.js
+++ api/routers/studentRouter.js
@@ -10,7 +10,7 @@ router.delete("/:id", StudentsController.delete); // Delete
 router.get("/major/:majorId", StudentsController.getStudentsByMajor); // Get students by major
 router.get("/performance/:studentId", StudentsController.getStudentPerformance); // Get student's academic performance
 router.post("/login", StudentsController.login); // Login student
-router.get("/isGraduate/:studentId", StudentsController.isGraduate); // Check if student is graduate
+router.get("/isExpectedToGraduate/:studentId", StudentsController.isGraduate); // Check if student is graduate
 router.get("/isPreGraduate/:studentId", StudentsController.isPreGraduate); // Check if student is pregraduate
 
 module.exports = router;
